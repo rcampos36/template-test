e23cf7b1e933b61c93bf401baecd6871519a40f3
Roger Campos <rcrogercampos@gmail.com>
2025-07-14 14:17:16 -0400

    new quote project

---
+++ script.js
@@ -0,0 +1,70 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+// show loading 
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// hide loading 
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+// show new quote
+function newQuote() {
+  // pick a random quote from apiQuotes array
+  const quote = apiQuotes [Math.floor(Math.random() * apiQuotes.length)];
+
+  //check if author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  //check quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+
+  // set the quote, hide loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// get quotes from api
+async function getQuotes() {
+  loading();
+  const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    //catch error here
+  }
+}
+
+//tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// on load
+getQuotes();
